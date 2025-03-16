1d7c99c0c567a4bbd94d5e720c248c809d1664d3
Javi883 <requin883@gmail.com>
2025-03-16 00:43:48 -0400

    Create userPrompts and taskListName

---
+++ data.js
@@ -0,0 +1,12 @@
+export const userPrompts = [
+  'Please make a selection from the following options:',
+  '1. View all tasks',
+  '2. View all tasks done',
+  '3. View all pending tasks',
+  '4. Add a task',
+  '5. Edit a task',
+  '6. Delete a task',
+  '7. Exit',
+]
+
+export const taskListName = 'tasks.txt';
